73ca6afbd88616180cb7223667c61b701382ade8
Robert Adrian Bucur <adrian_bucur49@ymail.com>
2020-03-24 11:30:32 +0200

    Made the window not resizable; modified the icon to custom one; made the minimize button with System Tray.

--- Calculator/CalculatorWindow.xaml.cs
+++ Calculator/CalculatorWindow.xaml.cs
@@ -19,9 +19,35 @@ namespace Calculator
     /// </summary>
     public partial class CalculatorWindow : Window
     {
+        System.Windows.Forms.NotifyIcon notifyIcon = new System.Windows.Forms.NotifyIcon();
         public CalculatorWindow()
         {
             InitializeComponent();
+        }
+        private void WindowMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+        private void RestoreWindowDoubleClick(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            notifyIcon.Visible = false;
+            this.ShowInTaskbar = true;
+            SystemCommands.RestoreWindow(this);
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+        private void MinimizeButtonClick(object sender, RoutedEventArgs e)
+        {
+            SystemCommands.MinimizeWindow(this);
+            notifyIcon.Icon = new System.Drawing.Icon("Calculator.ico");
+            notifyIcon.Visible = true;
+            notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(RestoreWindowDoubleClick);
+            this.ShowInTaskbar = false;
+            notifyIcon.BalloonTipText = "The application is now down in the System Tray!";
+            notifyIcon.ShowBalloonTip(500);
+        }
+        private void ExitButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
